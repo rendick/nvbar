bcdd4c5ed4d314fcf0d11ae7d621ed461f59b8e5
rendick <rendick123@proton.me>
2024-01-06 12:50:39 +0100

    Prettier

--- main.go
+++ main.go
@@ -9,81 +9,94 @@ import (
 	"time"
 )
 
+var DateOutput string
+var MemoryOutput string
+var NetworkOutput string
+var KeyboardOutput string
+var BatteryOutput string
+
+func Date() {
+	DateOutput = fmt.Sprintf("%s", time.Now().Format("2006-01-02 15:04:05"))
+}
+
+func Memory() {
+	avail_memory, err_avail := exec.Command("sh", "-c", "cat /proc/meminfo | grep 'MemAvailable:' | awk '{print $2}'").Output()
+	if err_avail != nil {
+		os.Exit(0)
+	}
+	total_memory, err_total := exec.Command("sh", "-c", "cat /proc/meminfo | grep 'MemTotal:' | awk '{print $2}'").Output()
+	if err_total != nil {
+		os.Exit(0)
+	}
+
+	avail_convert := strings.TrimSpace(string(avail_memory))
+	avail, err_convert := strconv.Atoi(avail_convert)
+	if err_convert != nil {
+		panic(err_avail)
+	}
+
+	total_convert := strings.TrimSpace(string(total_memory))
+	total, err_convert_total := strconv.Atoi(total_convert)
+	if err_convert_total != nil {
+		panic(err_total)
+	}
+
+	MemoryOutput = fmt.Sprintf("%d MB / %d MB", (total-avail)/1024, total/1024)
+}
+
+func Network() {
+	network, err := exec.Command("sh", "-c", "ping -q -c1 google.com &>/dev/null && echo online || echo offline").Output()
+	if err != nil {
+		panic(err)
+	}
+	NetworkOutput = fmt.Sprintf("%s", strings.Replace(string(network), "\n", "", -1))
+}
+
+func Keyboard() {
+	keyboard, err := exec.Command("xkb-switch").Output()
+	if err != nil {
+		panic(err)
+	}
+	KeyboardOutput = fmt.Sprintf("%s", strings.Replace(string(keyboard), "\n", "", -1))
+}
+
+func Battery() {
+	batteryDir, err := os.Open("/sys/class/power_supply/")
+	if err != nil {
+		panic(err)
+	}
+	batteryFiles, err := batteryDir.Readdir(-1)
+	batteryDir.Close()
+	if err != nil {
+		panic(err)
+	}
+	for _, file := range batteryFiles {
+		if strings.HasPrefix(file.Name(), "BAT") {
+
+			batteryCmd := exec.Command("cat", fmt.Sprintf("/sys/class/power_supply/%s/capacity", file.Name()))
+			batteryOutputBytes, err := batteryCmd.Output()
+			if err != nil {
+				panic(err)
+			}
+			BatteryOutput = strings.TrimSpace(string(batteryOutputBytes)) + " "
+		}
+	}
+}
+
 func main() {
 	for {
-		// Time
-		// date := time.Now().Format(time.RFC850)
-		date := time.Now()
 
-		// CPU usage
-		// cpu, err := exec.Command("sh", "-c", "echo "CPU Usage: "$[100-$(vmstat 1 2|tail -1|awk '{print $15}')]"%"")
-
-		// Memory
-
-		avail_memory, err_avail := exec.Command("sh", "-c", "cat /proc/meminfo | grep 'MemAvailable:' | awk '{print $2}'").Output()
-		if err_avail != nil {
-			os.Exit(0)
-		}
-		total_memory, err_total := exec.Command("sh", "-c", "cat /proc/meminfo | grep 'MemTotal:' | awk '{print $2}'").Output()
-		if err_total != nil {
-			os.Exit(0)
-		}
-
-		avail_convert := strings.TrimSpace(string(avail_memory))
-		avail, err_convert := strconv.Atoi(avail_convert)
-		if err_convert != nil {
-			panic(err_avail)
-		}
-
-		total_convert := strings.TrimSpace(string(total_memory))
-		total, err_convert_total := strconv.Atoi(total_convert)
-		if err_convert_total != nil {
-			panic(err_total)
-		}
-
-		// Connection
-		connection, err := exec.Command("sh", "-c", "ping -q -c1 google.com &>/dev/null && echo online || echo offline").Output()
-		if err != nil {
-			panic(err)
-		}
-
-		// Keyboard layout
-		keyboard, err := exec.Command("xkb-switch").Output()
-		if err != nil {
-			panic(err)
-		}
-
-		// Battery
-		var batteryOutput string
-
-		batteryDir, err := os.Open("/sys/class/power_supply/")
-		if err != nil {
-			panic(err)
-		}
-		batteryFiles, err := batteryDir.Readdir(-1)
-		batteryDir.Close()
-		if err != nil {
-			panic(err)
-		}
-		for _, file := range batteryFiles {
-			if strings.HasPrefix(file.Name(), "BAT") {
-
-				batteryCmd := exec.Command("cat", fmt.Sprintf("/sys/class/power_supply/%s/capacity", file.Name()))
-				batteryOutputBytes, err := batteryCmd.Output()
-				if err != nil {
-					panic(err)
-				}
-
-				batteryOutput += strings.TrimSpace(string(batteryOutputBytes)) + " "
-			}
-		}
-
-		fmt.Printf("%s"+" | "+"%d MB / %d MB"+" | "+"%s"+"| "+"%s"+" | "+"%s\n",
-			strings.Replace(string(connection), "\n", "", -1),
-			(total-avail)/1024, total/1024,
-			strings.Replace(string(keyboard), "\n", " ", -1),
-			strings.TrimSpace(batteryOutput),
-			date.Format("2006-01-02 15:04:05"))
+		Network()
+		Memory()
+		Keyboard()
+		Battery()
+		Date()
+		fmt.Printf("%s"+" | "+"%s"+" | "+"%s"+" | "+"%s"+" | "+"%s\n",
+			NetworkOutput,
+			MemoryOutput,
+			KeyboardOutput,
+			strings.TrimSpace(BatteryOutput),
+			DateOutput)
 
 		time.Sleep(time.Second)
 
